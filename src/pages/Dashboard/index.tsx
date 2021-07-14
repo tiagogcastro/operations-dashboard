c28b2390f2da8b365d2781bda87e48a8871122b7
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-14 12:55:31 -0300

    feat: Starting dashboard page

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+
+import { useAuth } from '../../contexts/AuthContext';
+import { api } from '../../services/api';
+
+import { Header } from '../../components/Header';
+import { SubHeader } from '../../components/SubHeader';
+
+import {
+  Container,
+  Content
+} from './styles';
+
+type User = {
+  email?: string;
+  id?: string;
+  imagem: string;
+  nome: string;
+}
+
+export function Dashboard() {
+  const history = useHistory();
+  const location = useLocation();
+  const {signInWithGoogle, token} = useAuth();
+  const paramsToken = location.search.replace('?jwt=', '');
+
+  const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    if(paramsToken) {
+      signInWithGoogle(paramsToken);
+      return;
+    } else if(!token) {
+      history.push('/');
+      return;
+    }
+    // api.get(`/operacoes`).then(response => {
+    //   console.log(response.data);
+    // });
+
+    api.get('/usuario').then(response => {
+      setUser(response.data);
+    });
+  }, [paramsToken, token, history, signInWithGoogle]);
+
+  return (
+    <Container>
+      <Header 
+        nome={user?.nome}
+        imagem={user?.imagem}
+      />
+      <SubHeader />
+      <Content>
+        
+      </Content>
+    </Container>
+  )
+}
