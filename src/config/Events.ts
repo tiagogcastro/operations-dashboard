0bfa39f4213dcb0d6d9cfbddd08871ad6c538ed8
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-15 22:25:41 -0300

    feat: Listing events

---
+++ src/config/Events.ts
@@ -0,0 +1,250 @@
+export default
+{
+  "cei": {
+    "operacoes": {
+      "Compra": "C",
+      "Venda": "V",
+      "Atualização": "SUB",
+      "Bonificação em Ativos": "BN",
+      "Desdobro": "S/I",
+      "Grupamento": "S/I",
+      "Incorporação": "AT",
+      "Leilão de Fração": "V.FRACAO"
+    },
+    "proventos": {
+      "Bonificação em Ativos": "BONIFICACAO",
+      "Juros Sobre Capital Próprio": "JSCP",
+      "Rendimento": "RENDIMENTO",
+      "Leilão de Fração": "V.FRACAO"
+    }
+  },
+  "sistema": {
+    "operacoes": {
+      "Compra": "C",
+      "Venda": "V",
+      "Daytrade": "DT",
+      "Exercício de Subscrição": "SUB",
+      "Venda Direito de Subscrição": "V.SUB",
+      "Bonificação em Ativos": "BN",
+      "Oferta Publica Inicial": "IPO",
+      "Desdobro": "S/I",
+      "Grupamento": "S/I",
+      "Alteração de Ticker": "AT",
+      "Transferência de Custódia": "TC",
+      "Baixa": "BAIXA",
+      "Come Cota": "C.COTA",
+      "Venda de Frações": "V.FRACAO",
+      "V.SOB": "V.FRACAO",
+      "Restituição de Capital": "R.CAP",
+      "Amortização": "AM",
+      "Cisão": "CISÃO",
+      "Ajuste de Posição": "AJ.POS",
+      "Exercício de Opção": "EX.OPC",
+      "Termo": "TERMO",
+      "Isento": "ISENTO",
+      "Taxa": "TAXA"
+    },
+    "proventos": {
+      "Bonificação em Ativos": "BONIFICACAO",
+      "Juros Sobre Capital Próprio": "JSCP",
+      "Rendimento": "RENDIMENTO",
+      "V.SOB(BN)": "V.FRACAO",
+      "JRSCAPPROPRIO": "JSCP"
+    }
+  },
+  "planilha": {
+    "operacoes": [
+      "",
+      "C",
+      "V",
+      "DT",
+      "SUB",
+      "V.SUB",
+      "BN",
+      "IPO",
+      "S/I",
+      "AT",
+      "TC",
+      "BAIXA",
+      "C.COTA",
+      "V.FRACAO",
+      "R.CAP",
+      "AM",
+      "CISÃO",
+      "AJ.POS",
+      "EX.OPC",
+      "TERMO",
+      "ISENTO",
+      "TAXA"
+    ],
+    "proventos": [
+      "DIVIDENDO",
+      "JRS CAP PROPRIO",
+      "JSCP",
+      "RENDIMENTO",
+      "BONIFICACAO",
+      "BTC",
+      "V.SOB(BN)",
+      "V.FRACAO",
+      "REEMBOLSO(DIV)",
+      "REEMBOLSO(JSCP)",
+      "CRED.NAO.PAGO(JSCP)"
+    ]
+  },
+  "moedas": [
+    "AUD",
+    "BRL",
+    "CAD",
+    "CHF",
+    "DKK",
+    "EUR",
+    "GBP",
+    "GBX",
+    "JPY",
+    "NOK",
+    "SEK",
+    "USD"
+  ],
+  "corretoras": {
+    "Agora": {
+      "nome": "Agora",
+      "cnpj": "74014747000135",
+      "razao_social": "AGORA CTVM S.A."
+    },
+    "Ativa": {
+      "nome": "Ativa",
+      "cnpj": "33775974000104",
+      "razao_social": "ATIVA INVESTIMENTOS S.A. C.T.C.V."
+    },
+    "BB": {
+      "nome": "BB",
+      "cnpj": "24933830000130",
+      "razao_social": "BB-BANCO DE INVESTIMENTO S.A."
+    },
+    "BTG": {
+      "nome": "BTG",
+      "cnpj": "43815158000122",
+      "razao_social": "BTG PACTUAL CTVM S/A"
+    },
+    "C6Bank": {
+      "nome": "C6Bank",
+      "cnpj": "31872495000172",
+      "razao_social": "Banco C6 - SA"
+    },
+    "CEF": {
+      "nome": "CEF",
+      "cnpj": "00360305000104",
+      "razao_social": "CAIXA ECONOMICA FEDERAL"
+    },
+    "Clear": {
+      "nome": "Clear",
+      "cnpj": "02332886001178",
+      "razao_social": "CLEARCORRETORA-GRUPOXP"
+    },
+    "CMCapital": {
+      "nome": "CMCapital",
+      "cnpj": "02685483000130",
+      "razao_social": "CM CAPITAL MARKETS CCTVM LTDA "
+    },
+    "Easynvest": {
+      "nome": "Easynvest",
+      "cnpj": "62169875000179",
+      "razao_social": "EASYNVEST - TITULO CORRETORA DE VALORES SA"
+    },
+    "Genial": {
+      "nome": "Genial",
+      "cnpj": "27652684000162",
+      "razao_social": "GENIAL INVESTIMENTOS CORRETORA DE VALORES MOBILIARIOS S.A."
+    },
+    "Guide": {
+      "nome": "Guide",
+      "cnpj": "065913436000117",
+      "razao_social": "Guide Investimentos Sa Corretora de Valores"
+    },
+    "Inter": {
+      "nome": "Inter",
+      "cnpj": "18945670000146",
+      "razao_social": "INTER DISTRIBUIDORA DE TITULOS E VALORES MOBILIARIOS"
+    },
+    "Itau": {
+      "nome": "Itau",
+      "cnpj": "61194353000164",
+      "razao_social": "ITAU CORRETORA DE VALORES S/A"
+    },
+    "Kirton": {
+      "nome": "Kirton",
+      "cnpj": "58229246000110",
+      "razao_social": "Bradesco-kirton Corretora de Cambio S.A."
+    },
+    "Mirae": {
+      "nome": "Mirae",
+      "cnpj": "12392983000138",
+      "razao_social": "MIRAE ASSET WEALTH MANAGEMENT (BRAZIL) CCTVM LTDA"
+    },
+    "Modal": {
+      "nome": "Modal",
+      "cnpj": "30723886000162",
+      "razao_social": "BANCO MODAL S.A."
+    },
+    "ModalMais": {
+      "nome": "ModalMais",
+      "cnpj": "05389174000101",
+      "razao_social": "MODAL D.T.V.M. LTDA"
+    },
+    "Necton": {
+      "nome": "Necton",
+      "cnpj": "52904364000108",
+      "razao_social": "Necton Investimentos S.A."
+    },
+    "NovaFutura": {
+      "nome": "NovaFutura",
+      "cnpj": "04257795000179",
+      "razao_social": "NOVA FUTURA CTVM LTDA"
+    },
+    "Orama": {
+      "nome": "Orama",
+      "cnpj": "13293225000125",
+      "razao_social": "ORAMA DTVM"
+    },
+    "Rico": {
+      "nome": "Rico",
+      "cnpj": "02332886001682",
+      "razao_social": "RICO INVESTIMENTOS - GRUPO XP"
+    },
+    "Santander": {
+      "nome": "Santander",
+      "cnpj": "51014223000149",
+      "razao_social": "Santander Corretora de Cambio e Valores Mobiliarios S.A."
+    },
+    "Socopa": {
+      "nome": "Socopa",
+      "cnpj": "62285390000140",
+      "razao_social": "SOCOPA SOCIEDADE CORRETORA PAULISTA SA"
+    },
+    "Terra": {
+      "nome": "Terra",
+      "cnpj": "03751794000113",
+      "razao_social": "TERRA INVESTIMENTOS DISTRIBUIDORA DE TITULOS E VALORES MOBILIARIOS LTDA"
+    },
+    "Toro": {
+      "nome": "Toro",
+      "cnpj": "29162769000198",
+      "razao_social": "TORO CORRETORA DE TITULOS E VALORES MOBILIARIOS LTDA"
+    },
+    "Vitreo": {
+      "nome": "Vitreo",
+      "cnpj": "34711571000156",
+      "razao_social": "Vitreo Distribuidora de Títulos e Valores Mobiliários S.A."
+    },
+    "Warren": {
+      "nome": "Warren",
+      "cnpj": "92875780000131",
+      "razao_social": "Warren Corretora de Valores Mobiliarios e Cambio LTDA."
+    },
+    "XP": {
+      "nome": "XP",
+      "cnpj": "02332886000104",
+      "razao_social": "XP INVESTIMENTOS CCTVM S.A."
+    }
+  }
+}
