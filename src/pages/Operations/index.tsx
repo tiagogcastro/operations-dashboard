4929979f3c2469b0c57a744f6b99a35a570ee7c1
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-15 22:24:54 -0300

    feat: Created operations insert and styled

--- src/pages/Operations/index.tsx
+++ src/pages/Operations/index.tsx
@@ -1,13 +1,159 @@
+import react, { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
+import jspreadsheet from 'jspreadsheet-ce';
+import "../../../node_modules/jspreadsheet-ce/dist/jspreadsheet.css";
+import { useAuth } from '../../contexts/AuthContext';
+import { api } from '../../services/api';
+
+import { Header } from '../../components/Header';
+import { SubHeader } from '../../components/SubHeader';
+
+import Events from '../../config/Events';
+
 import {
   Container,
   Content
 } from './styles';
 
+type Operation = {
+  ativo: string
+  corretora: string
+  date: Date;
+  evento: string;
+  id_op: number;
+  irrf: string;
+  moeda: string;
+  observacao: string;
+  preco: number;
+  qtd: number;
+  taxas: string;
+}
+
 export function Operations() {
+  const { user } = useAuth();
+  const [newOperations, setNewOperations] = useState<Operation[]>();
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState('');
+  
+  const eventos = Events.planilha.operacoes;
+  const corretoras = (Object.keys(Events.corretoras)).toString().toUpperCase().split(",");
+  const moedas = Events.moedas;
+
+  const options: any = {
+    minDimensions: [1, 1],
+    columns: [
+      { title: "ativo", type: "text", width: 150 },
+      { title: "date", type: "calendar", width: 120 },
+      { title: "evento", type: "dropdown", width: 120, source: eventos },
+      { title: "qtd", type: "text", width: 120 },
+      { title: "preco", type: "text", width: 120 },
+      { title: "taxas", type: "text", width: 120 },
+      { title: "corretora", type: "dropdown", width: 100, source: corretoras },
+      { title: "irrf", type: "text", width: 120 },
+      { title: "moeda", type: "dropdown", width: 120, source: moedas },
+      { title: "observacao", type: "text", width: 120 }
+    ],
+    columnSorting: true,
+    // pagination: 10,
+    search: false,
+    filters: false,
+    allowComments: false
+  };
+
+  const jRef: any = useRef(null);
+
+  useEffect(() => {
+    if (!jRef.current.jspreadsheet) {
+      jspreadsheet(jRef.current, options)
+    }
+  }, [options]);
+
+  const addRow = () => {
+    jRef.current.jexcel.insertRow();
+  };
+
+  const handleSendNewOperations = useCallback(async (operations: Operation[]) => {
+    console.log(operations);
+    const response = await api.post('/operacoes', operations);
+    console.log(response.data);
+  }, []);
+  
+  const insert = async () => {
+    var table = jRef.current.jexcel.table;
+
+    var xml_tr = table.getElementsByTagName("tr");
+    var result: Operation[] = [];
+    var header: any[] = [];
+
+    for (var i = 0; i < xml_tr.length; i++) {
+      var xml_td = xml_tr[i].getElementsByTagName("td");
+      var row: any = {};
+
+      for (var j = 0; j < xml_td.length; j++) {
+        let value = xml_td[j].childNodes[0];
+        if (value) {
+          value = (value.nodeValue).replace('.', '').replace(',', '.');
+          if (value.indexOf("/") > 0) {
+            value = value.slice(0, 10).split("/").reverse().join("-");
+          } else {
+            value = parseFloat(value) || value
+          }
+        } else {
+          value = "";
+        }
+
+        if (i === 0) {
+          if (j === 0) {
+            value = "id_op";
+          }
+          header.push(value);
+        } else {
+          row[header[j]] = value;
+          if(!value) {
+            setError('Preencha todos os campos.');
+            return;
+          } 
+        }
+      }
+
+      if (i === 0) {
+        continue;
+      }
+      if (row.ativo) {
+        result.push(row);
+      }
+    }
+
+    if(result.length === 0) {
+      return;
+    }
+    setNewOperations(result);
+    console.log(result);
+    setLoader(true)
+    const response = await api.post('/operacoes', result);
+    setLoader(false);
+    setError('');
+    console.log(response.data);
+  }
+
   return (
     <Container>
+      <Header 
+        nome={user?.nome}
+        imagem={user?.imagem}
+      />
+      <SubHeader />
       <Content>
-
+        <section>
+          <div>
+            <h1>Operações</h1>
+            <aside>
+              <button  type="button" onClick={addRow}> Adicionar linha </button>
+              <button type="button" disabled={loader} onClick={insert}> {loader ? 'Carregando...': 'Inserir Operações'} </button>
+            </aside>
+            {error && <p>{error}</p>}
+          </div>
+          <div ref={jRef} id="jspreadsheet" />
+        </section>
       </Content>
     </Container>
   );
--- src/pages/Operations/styles.ts
+++ src/pages/Operations/styles.ts
@@ -1,9 +1,105 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-
+  height: 100%;
+  width: 100%;
 `;
 
 export const Content = styled.div`
+  width: 100%;
+  max-width: 1260px;
+  margin: 0 auto;
+  height: 100%;
 
+  section {
+    margin: 64px auto;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+
+    > div {
+      width: 100%;
+      > h1 {
+        font-size: 3.2rem;
+      }
+
+      > p {
+        color: red;
+        font-size: 1.6rem;
+        text-align: right;
+        margin: 1rem 0;
+      }
+
+      aside {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+        align-items: center;
+        > button {
+          font-size: 1.6rem;
+          background: var(--semi-blue);
+          color: var(--semi-white);
+          border-radius: 8px;
+          padding: 8px 14px;
+          margin: 2rem 0 1rem;
+          transition: filter 0.2s;
+
+          &:hover {
+            filter: brightness(90%);
+          }
+        }
+      }
+    }
+
+    #jspreadsheet {
+      .jdropdown-container {
+        background: var(--semi-blue);
+        font-size: 3rem;
+
+        .jdropdown-content {
+          z-index: 10;
+          border-radius: 0 0 8px 8px;
+          box-shadow: 2px 2px 10px rgba(0, 0, 0, 0.1);
+          .jdropdown-item {
+            font-size: 2.6rem;
+            padding: 10px 0;
+            cursor: pointer;
+
+            transition: background 0.2s;
+
+            &:hover {
+              background: #c4c4c4;
+            }
+          }
+        }
+
+      }
+      table {
+        thead {
+          tr {
+            font-size: 3rem;
+          }
+        }
+        
+        tbody {
+          tr {
+            font-size: 3rem;
+            td {
+              z-index: 5;
+              .jcalendar.jcalendar-container.jcalendar-focus {
+                position: absolute;
+                background: var(--semi-white);
+                border: 1px solid var(--font-cyan-blue)
+              }
+            }
+
+            td.editor {
+              font-size: 3rem;
+            }
+          }
+        }
+      }
+    }
+  }
 `;
