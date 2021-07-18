0890e86ea284ba0aa5224710fe7fc4086bde92de
Tiago Gonçalves <tiaguin180@gmail.com>
2021-07-18 17:46:13 -0300

    feat: Created Input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useRef } from 'react';
+import { useField } from '@unform/core';
+
+export const Input = ({ name, type, ...rest }: any) => {
+  const inputRef = useRef(null)
+
+  const { fieldName, registerField } = useField(name)
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef,
+      getValue: ref => {
+        return ref.current.value
+      },
+      setValue: (ref, value) => {
+        ref.current.value = value
+      },
+      clearValue: ref => {
+        ref.current.value = ''
+      },
+    })
+  }, [fieldName, registerField])
+
+  useEffect(() => {}, [])
+  return (
+    <input
+      name={name}
+      ref={inputRef}
+      type={type}
+      placeholder="Type your username"
+      {...rest}
+    />
+  );
+}
